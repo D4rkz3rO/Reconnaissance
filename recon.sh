b89053fe3bda17e2bbd1e471a77416dcc3eec752
memN0ps <37394329+memN0ps@users.noreply.github.com>
2019-11-23 13:05:48 +1300

    Recon
    Recon bash script for subdomain enumeration

---
+++ recon.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#Author memN0ps
+
+# Starting amass with configuration script
+amass enum -passive -config ~/config.ini -d $1 -o amass_subdomains.txt
+
+# Starting subfinder
+~/go/bin/subfinder -d $1 -o subfinder_subdomains.txt
+
+# Starting assetfinder
+~/go/bin/assetfinder $1 | tee -a assetfinder_subdomains.txt
+
+# Starting sublist3r
+~/tools/Sublist3r/sublist3r.py -d $1 -o sublist3r_subdomains.txt
+
+# Merging subdomains
+cat amass_subdomains.txt >> merge.txt && cat subfinder_subdomains.txt >> merge.txt && cat assetfinder_subdomains.txt >> merge.txt && cat sublist3r_subdomains.txt >> merge.txt
+
+# Removing duplicate entries
+sort -u merge.txt -o all_subdomains.txt
+
+# Starting httpprobe
+cat all_subdomains.txt | ~/go/bin/httprobe | tee -a alive.txt
+
+# Starting gowitness
+#~/go/bin/gowitness file --source=urls.txt --threads=4 --resolution="1200,750" --log-format=json --log-level=warn --timeout=60 --destination="gowitness_screenshots"
+
+# Starting aquatone
+cat all_subdomains.txt | ~/tools/aquatone/aquatone -out aquatone
