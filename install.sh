5af374dd92c45d566511c90604f63772169a0d20
memN0ps <37394329+memN0ps@users.noreply.github.com>
2019-12-01 17:01:02 +1300

    Updated Install.sh
    Added dirsearcher, added echo's

--- install.sh
+++ install.sh
@@ -2,28 +2,49 @@
 
 #Author memN0ps
 
-#Installing Amass
+#Installing amass
+echo "Installing amass"
 export GO111MODULE=on
 go get -v -u github.com/OWASP/Amass/v3/...
+echo "Done! amass installed"
 
 #Installing subfinder
+echo "Installing subfinder"
 go get -v -u github.com/subfinder/subfinder
+echo "Done! subfinder installed"
 
 #Installing aquatone
+echo "Installing aquatone"
 go get -v -u github.com/michenriksen/aquatone
+echo "Done! aquatone installed"
 
 #Installing assetfinder
+echo "Installing assetfinder"
 go get -v -u github.com/tomnomnom/assetfinder
+echo "Done! assetfinder installed"
 
 #Installing httprobe
+echo "Installing httprobe"
 go get -v -u github.com/tomnomnom/httprobe
+echo "Done! httprobe installed"
 
 #Installing gowitness
+echo "Installing gowitness"
 go get -v -u github.com/sensepost/gowitness
+echo "Done! gowitness installed"
 
 #Installing gobuster
+echo "Installing gobuster"
 go get -v -u github.com/OJ/gobuster
+echo "Done! gobuster installed"
 
-#Installing Sublist3r
+#Installing sublist3r
+echo "Installing sublist3r"
 git clone https://github.com/aboul3la/Sublist3r.git ~/tools/Sublist3r/
 pip install -r ~/tools/Sublist3r/requirements.txt
+echo "Done! sublist3r installed"
+
+#Installing dirsearch
+echo "Installing dirsearch"
+git clone https://github.com/maurosoria/dirsearch.git ~/tools/dirsearch/
+echo "Done! dirsearch installed"
